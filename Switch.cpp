070303411541ac3416948697adac1b8190f749d8
KyleNi <kylejessie27@gmail.com>
2025-12-07 00:19:42 +0800

    Implement switch statement for grading system

---
+++ Switch.cpp
@@ -0,0 +1,15 @@
+char grade = 'B';
+
+switch (grade) {
+    case 'A':
+        cout << "Excellent!";
+        break;
+    case 'B':
+        cout << "Good!";
+        break;
+    case 'C':
+        cout << "Pass";
+        break;
+    default:
+        cout << "Fail";
+}
