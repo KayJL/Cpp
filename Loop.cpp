2df59341186e92c078a6696f8a33fdcc2048a73a
KyleNi <kylejessie27@gmail.com>
2025-12-07 00:19:21 +0800

    Create Loop.cpp

---
+++ Loop.cpp
@@ -0,0 +1,17 @@
+//Loop
+for (int i = 1; i <= 10; i++) {
+    cout << i << " ";
+}
+//While Loop
+int x = 0;
+while (x < 5) {
+    cout << "Hello\n";
+    x++;
+}
+//Do While Loop
+int n = 1;
+
+do {
+    cout << n << " ";
+    n++;
+} while (n <= 5);
