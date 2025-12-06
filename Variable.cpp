165dd1ce7a27d16bd9560b8b02eeabfb60f922df
KyleNi <kylejessie27@gmail.com>
2025-12-07 00:13:52 +0800

    Create Variable.cpp

---
+++ Variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    double height = 5.9;
+    string name = "Kay";
+
+    cout << name << " is " << age << " years old.";
+    return 0;
+}
