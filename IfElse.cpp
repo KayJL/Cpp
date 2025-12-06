ed7222d38e570b3b7f91a83b03d4b7bb4725036d
KyleNi <kylejessie27@gmail.com>
2025-12-07 00:17:20 +0800

    Create IfElse.cpp

---
+++ IfElse.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "You are an adult!";
+    } else {
+        cout << "You are a minor!";
+    }
+
+  
+if (score >= 90) {
+    cout << "A";
+} else if (score >= 80) {
+    cout << "B";
+} else if (score >= 70) {
+    cout << "C";
+} else {
+    cout << "F";
+}
+ return 0;
+}
